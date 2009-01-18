1a79bc5fb089a13874eb5ca3a81ff98632a990db
pyro <pyro@DualCore64.hsd1.nj.comcast.net>
2009-01-17 21:16:17 -0500

    1. add ReturnType and LocalVariables to the Procedure class 2. put references to the input variables in LocalVariables also

--- tt_procedure.py
+++ tt_procedure.py
@@ -4,17 +4,23 @@
 # Copyright (c) 2009 by Richard Goedeken
 #--------------------------------------------------
 
-from tt_variable import *
-
 class Procedure:
     def __init__(self, Name, InputVars):
         self.CodeText = ""
         self.Name = Name
+        self.ReturnType = ParamType.UNKNOWN
         self.InputVariables = []
+        self.LocalVariables = []
         self.Instructions = []
         for varname in InputVars:
             newvar = Variable(varname)
-            self.InputVariables.append(newvar)
+            self.InputVariables.append(newvar)      # references to the new Variable object are stored in both
+            self.LocalVariables.append(newvar)      # InputVariables and LocalVariables lists
             
     def AddCode(self, SourceCode):
         self.CodeText = SourceCode
+
+from tt_variable import *
+from tt_types import *
+
+
