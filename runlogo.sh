64e66e5caa4765a6395187be6d810bd054331d64
pyro <pyro@DualCore64.hsd1.nj.comcast.net>
2009-02-01 19:56:48 -0500

    changed runlogo.sh to put the intermediate build files in the same directory as the .logo source, but still put the final executable in the current (turboturtle) directory

--- runlogo.sh
+++ runlogo.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-name=`basename $1`
+name=`basename "$1"`
+destdir=`dirname "$1"`
 basename=${name%%.*}
 
 if [ ! -f $1 ]; then
@@ -7,21 +8,24 @@ if [ ! -f $1 ]; then
   exit 1
 fi
 
+includedir=`pwd`
+cd "$destdir"
+
 cppname=$basename.cpp
-echo "Compiling $1 to $cppname"
-./turboturtle.py $1 $cppname
+echo "Compiling $name to $cppname"
+"$includedir"/turboturtle.py $name $cppname
 if [ $? != 0 ]; then exit 2; fi
 
 objname=$basename.o
 echo "Compiling $cppname to $objname"
-g++ -o $objname -O3 -I/usr/include/SDL -c $cppname
+g++ -o $objname -O3 -I/usr/include/SDL -I"$includedir" -c $cppname
 if [ $? != 0 ]; then exit 3; fi
 
 echo "Compiling wrapper code"
-g++ -o wrapper.o -O3 -I/usr/include/SDL -c wrapper_main.cpp
+g++ -o "$includedir"/wrapper.o -O3 -I/usr/include/SDL -I"$includedir" -c "$includedir"/wrapper_main.cpp
 if [ $? != 0 ]; then exit 4; fi
 
-exename=$basename
-echo "Linking wrapper code with $objname to produce executable."
-g++ -o $exename -lSDL -lGL -lm $objname wrapper.o
+exename="$includedir"/$basename
+echo "Linking wrapper code with $objname to produce executable $exename."
+g++ -o $exename -lSDL -lGL -lm $objname "$includedir"/wrapper.o
 
