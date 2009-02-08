9eef4a9157d6511e1eabd19eb4ec54b613a572de
pyro <pyro@DualCore64.hsd1.nj.comcast.net>
2009-02-07 23:39:04 -0500

    Added support for WRAP mode

--- wrapper_main.cpp
+++ wrapper_main.cpp
@@ -21,6 +21,8 @@ static bool bFullscreen = false;
 static bool bReturnWhenDone = false;
 static int iScreenWidth = 512;
 static int iScreenHeight = 512;
+static int iViewWidth = 0;
+static int iViewHeight = 0;
 static unsigned int uiClockFrameStart;
 
 // real global variables
@@ -220,7 +222,6 @@ bool InitGL(void)
     glLoadIdentity();
 
     // don't use perspective
-    int iViewWidth, iViewHeight;
     if (iScreenWidth <= iScreenHeight)
     {
         fPixelsPerTurtleStep = (float) iScreenWidth / (float) tt_WindowSize;
@@ -307,9 +308,251 @@ void wrapper_Erase(void)
 
 void wrapper_DrawLineSegment(float *pfOrigPos, float *pfNewPos, bool bWrapEnabled)
 {
+    if (!bWrapEnabled)
+    {
+        glVertex2f(pfOrigPos[0], pfOrigPos[1]);
+        glVertex2f(pfNewPos[0], pfNewPos[1]);
+        return;
+    }
+
+    float fWidthLimit = (float) iViewWidth / 2.0;
+    float fHeightLimit = (float) iViewHeight / 2.0;
+
+    float fStartX = fmodf(pfOrigPos[0] + fWidthLimit, (float) iViewWidth);
+    if (fStartX < 0.0) fStartX += (float) iViewWidth;
+    float fStartY = fmodf(pfOrigPos[1] + fHeightLimit, (float) iViewHeight);
+    if (fStartY < 0.0) fStartY += (float) iViewHeight;
+
+    float fDeltaX = pfNewPos[0] - pfOrigPos[0];
+    float fDeltaY = pfNewPos[1] - pfOrigPos[1];
+
+    float fEndX = fStartX + fDeltaX;
+    float fEndY = fStartY + fDeltaY;
+
+    while(true)
+    {
+        float fEdgeX, fEdgeY;
+        bool bWrapX = false, bWrapY = false;
+
+        if (fEndX < 0.0)
+        {
+            float fInterceptY = fStartY - (fStartX - 0.0) * fDeltaY / fDeltaX;
+            if (fInterceptY == 0.0)
+            {
+                fEdgeX = 0.0;
+                fEdgeY = 0.0;
+                bWrapX = bWrapY = true;
+            }
+            else if (fInterceptY > 0.0 and fInterceptY < (float) iViewHeight)
+            {
+                fEdgeX = 0.0;
+                fEdgeY = fInterceptY;
+                bWrapX = true;
+            }
+        }
+        if (fEndY > (float) iViewHeight)
+        {
+            float fInterceptX = fStartX + ((float) iViewHeight - fStartY) * fDeltaX / fDeltaY;
+            if (fInterceptX == 0.0)
+            {
+                fEdgeX = 0.0;
+                fEdgeY = (float) iViewHeight;
+                bWrapX = bWrapY = true;
+            }
+            else if (fInterceptX > 0.0 and fInterceptX < (float) iViewWidth)
+            {
+                fEdgeX = fInterceptX;
+                fEdgeY = (float) iViewHeight;
+                bWrapY = true;
+            }
+        }
+        if (fEndX > (float) iViewWidth)
+        {
+            float fInterceptY = fStartY + ((float) iViewWidth - fStartX) * fDeltaY / fDeltaX;
+            if (fInterceptY == (float) iViewHeight)
+            {
+                fEdgeX = (float) iViewWidth;
+                fEdgeY = (float) iViewHeight;
+                bWrapX = bWrapY = true;
+            }
+            else if (fInterceptY > 0.0 and fInterceptY < (float) iViewHeight)
+            {
+                fEdgeX = (float) iViewWidth;
+                fEdgeY = fInterceptY;
+                bWrapX = true;
+            }
+        }
+        if (fEndY < 0.0)
+        {
+            float fInterceptX = fStartX - (fStartY - 0.0) * fDeltaX / fDeltaY;
+            if (fInterceptX == (float) iViewWidth)
+            {
+                fEdgeX = (float) iViewWidth;
+                fEdgeY = 0.0;
+                bWrapX = bWrapY = true;
+            }
+            else if (fInterceptX > 0.0 and fInterceptX < (float) iViewWidth)
+            {
+                fEdgeX = fInterceptX;
+                fEdgeY = 0.0;
+                bWrapY = true;
+            }
+        }
+        // if there was no wrap, just draw the line and return
+        if (!bWrapX && !bWrapY)
+        {
+            glVertex2f(fStartX - fWidthLimit, fStartY - fHeightLimit);
+            glVertex2f(fEndX - fWidthLimit,   fEndY - fHeightLimit);
+            return;
+        }
+        // otherwise, draw a line segment
+        glVertex2f(fStartX - fWidthLimit, fStartY - fHeightLimit);
+        glVertex2f(fEdgeX - fWidthLimit,  fEdgeY - fHeightLimit);
+        // adjust the start and end points
+        if (bWrapX)
+        {
+            if (fEdgeX == 0.0)
+                fEndX += (float) iViewWidth;
+            else
+                fEndX -= (float) iViewWidth;
+            fStartX = (float) iViewWidth - fEdgeX;
+        }
+        else fStartX = fEdgeX;
+        if (bWrapY)
+        {
+            if (fEdgeY == 0.0)
+                fEndY += (float) iViewHeight;
+            else
+                fEndY -= (float) iViewHeight;
+            fStartY = (float) iViewHeight - fEdgeY;
+        }
+        else fStartY = fEdgeY;
+        // and loop again
+    } // while(true)
 }
 
 void wrapper_DrawLineSegment(double *pdOrigPos, double *pdNewPos, bool bWrapEnabled)
 {
+    if (!bWrapEnabled)
+    {
+        glVertex2d(pdOrigPos[0], pdOrigPos[1]);
+        glVertex2d(pdNewPos[0], pdNewPos[1]);
+        return;
+    }
+
+    double dWidthLimit = (double) iViewWidth / 2.0;
+    double dHeightLimit = (double) iViewHeight / 2.0;
+
+    double dStartX = fmod(pdOrigPos[0] + dWidthLimit, (double) iViewWidth);
+    if (dStartX < 0.0) dStartX += (double) iViewWidth;
+    double dStartY = fmod(pdOrigPos[1] + dHeightLimit, (double) iViewHeight);
+    if (dStartY < 0.0) dStartY += (double) iViewHeight;
+
+    double dDeltaX = pdNewPos[0] - pdOrigPos[0];
+    double dDeltaY = pdNewPos[1] - pdOrigPos[1];
+
+    double dEndX = dStartX + dDeltaX;
+    double dEndY = dStartY + dDeltaY;
+
+    while(true)
+    {
+        double dEdgeX, dEdgeY;
+        bool bWrapX = false, bWrapY = false;
+
+        if (dEndX < 0.0)
+        {
+            double dInterceptY = dStartY - (dStartX - 0.0) * dDeltaY / dDeltaX;
+            if (dInterceptY == 0.0)
+            {
+                dEdgeX = 0.0;
+                dEdgeY = 0.0;
+                bWrapX = bWrapY = true;
+            }
+            else if (dInterceptY > 0.0 and dInterceptY < (double) iViewHeight)
+            {
+                dEdgeX = 0.0;
+                dEdgeY = dInterceptY;
+                bWrapX = true;
+            }
+        }
+        if (dEndY > (double) iViewHeight)
+        {
+            double dInterceptX = dStartX + ((double) iViewHeight - dStartY) * dDeltaX / dDeltaY;
+            if (dInterceptX == 0.0)
+            {
+                dEdgeX = 0.0;
+                dEdgeY = (double) iViewHeight;
+                bWrapX = bWrapY = true;
+            }
+            else if (dInterceptX > 0.0 and dInterceptX < (double) iViewWidth)
+            {
+                dEdgeX = dInterceptX;
+                dEdgeY = (double) iViewHeight;
+                bWrapY = true;
+            }
+        }
+        if (dEndX > (double) iViewWidth)
+        {
+            double dInterceptY = dStartY + ((double) iViewWidth - dStartX) * dDeltaY / dDeltaX;
+            if (dInterceptY == (double) iViewHeight)
+            {
+                dEdgeX = (double) iViewWidth;
+                dEdgeY = (double) iViewHeight;
+                bWrapX = bWrapY = true;
+            }
+            else if (dInterceptY > 0.0 and dInterceptY < (double) iViewHeight)
+            {
+                dEdgeX = (double) iViewWidth;
+                dEdgeY = dInterceptY;
+                bWrapX = true;
+            }
+        }
+        if (dEndY < 0.0)
+        {
+            double dInterceptX = dStartX - (dStartY - 0.0) * dDeltaX / dDeltaY;
+            if (dInterceptX == (double) iViewWidth)
+            {
+                dEdgeX = (double) iViewWidth;
+                dEdgeY = 0.0;
+                bWrapX = bWrapY = true;
+            }
+            else if (dInterceptX > 0.0 and dInterceptX < (double) iViewWidth)
+            {
+                dEdgeX = dInterceptX;
+                dEdgeY = 0.0;
+                bWrapY = true;
+            }
+        }
+        // if there was no wrap, just draw the line and return
+        if (!bWrapX && !bWrapY)
+        {
+            glVertex2d(dStartX - dWidthLimit, dStartY - dHeightLimit);
+            glVertex2d(dEndX - dWidthLimit,   dEndY - dHeightLimit);
+            return;
+        }
+        // otherwise, draw a line segment
+        glVertex2d(dStartX - dWidthLimit, dStartY - dHeightLimit);
+        glVertex2d(dEdgeX - dWidthLimit,  dEdgeY - dHeightLimit);
+        // adjust the start and end points
+        if (bWrapX)
+        {
+            if (dEdgeX == 0.0)
+                dEndX += (double) iViewWidth;
+            else
+                dEndX -= (double) iViewWidth;
+            dStartX = (double) iViewWidth - dEdgeX;
+        }
+        else dStartX = dEdgeX;
+        if (bWrapY)
+        {
+            if (dEdgeY == 0.0)
+                dEndY += (double) iViewHeight;
+            else
+                dEndY -= (double) iViewHeight;
+            dStartY = (double) iViewHeight - dEdgeY;
+        }
+        else dStartY = dEdgeY;
+        // and loop again
+    } // while(true)
 }
 
