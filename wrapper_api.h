b658f27823268db376e3aecd20486a5d792f4361
pyro <pyro@DualCore64.hsd1.nj.comcast.net>
2009-02-17 22:53:51 -0500

    Added licensing comments in all the .cpp and .h files

--- wrapper_api.h
+++ wrapper_api.h
@@ -1,9 +1,21 @@
-
 /********************************************/
 /* Turbo Turtle Logo Core/Wrapper Interface */
 /*                                          */
 /*  Copyright (c) 2009 by Richard Goedeken  */
+/*     Richard@fascinationsoftware.com      */
 /********************************************/
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, version 3.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Functions and data in the compiled Logo C++ file
 void tt_LogoMain(void);
--- wrapper_clist.h
+++ wrapper_clist.h
@@ -2,7 +2,20 @@
 /* Turbo Turtle CList Wrapper Class         */
 /*                                          */
 /*  Copyright (c) 2009 by Richard Goedeken  */
+/*     Richard@fascinationsoftware.com      */
 /********************************************/
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, version 3.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdlib.h>
 #include <stdarg.h>
